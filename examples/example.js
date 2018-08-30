009ef6a6d4340aad31017c484c7f3faa2c36e415
Unknown <schorschdanzer@web.de>
2018-08-30 22:13:01 +0200

    Example to get token

--- examples/example.js
+++ examples/example.js
@@ -2,6 +2,6 @@ const Fryd = require('../src/index');
 
 const fryd = new Fryd(20, 10);
 
-fryd.getTrophiesFromList('<token>', '<id>')
+fryd.getTokenFromClientCred('<client_id>', '<client_secret>')
   .then((r) => console.log(r))
   .catch((e) => console.log(e));
